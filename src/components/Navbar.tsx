072616ce4461be7e3245e79f93666c5d97fed206
MisaJC <johncarlomisa399@gmail.com>
2025-01-14 11:14:07 +0800

    sync the user from clerk to the database

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
-// import { syncUser } from "@/actions/user.action";
+import { syncUser } from "@/actions/user.action";
 
 async function Navbar() {
   const user = await currentUser();
-  //   if (user) await syncUser();
+  if (user) await syncUser();
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
