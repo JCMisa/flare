817bfc8e3e0817354bd916e894c29b616d86f084
MisaJC <johncarlomisa399@gmail.com>
2025-01-16 17:33:16 +0800

    fetch posts and perform like, comment and delete for a post

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -57,7 +57,7 @@ export const getUserByClerkId = async (clerkId: string) => {
 
 export const getDbUserId = async () => {
   const { userId: clerkId } = await auth();
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
   if (!user) throw new Error("User not found");
@@ -68,6 +68,8 @@ export const getDbUserId = async () => {
 export const getRandomUsers = async () => {
   try {
     const userId = await getDbUserId();
+
+    if (!userId) return [];
 
     // get 3 random users excluding ourselves and user that already followed
     const randomUsers = await prisma.user.findMany({
